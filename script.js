3618a1226e1f10b606116dc9c50eec2e737339be
Ryan DeSha <rdesha0218@yahoo.com>
2015-09-26 17:27:05 -0700

    added function/prompt

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-for (var i = 0; i < 100; i++) {
-	if (i % 3 == 0 && i % 5 == 0) {
-		console.log("Fuzzbuzz!");
+//prompt user for a number
+//write one named function that takes an integer
+//parseInt()
+
+
+var num = prompt("Enter a non decimal number to count to:");
+
+
+function Fizzbuzz(num) {
+
+	function checkNum(num) {
+		if (num % 1 != 0) {
+			return "Invalid answer";
+		}
 	}
-	else if (i % 3 == 0) {
-		console.log("Fizz!");
+parseInt(num, 10);
+
+	for (var i = 0; i < num; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			console.log("Fizzbuzz!");
+		}
+		else if (i % 3 == 0) {
+			console.log("Fizz!");
+		}
+		else if (i % 5 == 0) {
+			console.log("Buzz!");
+		}
+		else {
+			console.log(i);
+			}
+		}
 	}
-	else if (i % 5 == 0) {
-		console.log("Buzz!")
-	}
-	else {
-		console.log(i);
-	}
-}
+
+Fizzbuzz(num);
